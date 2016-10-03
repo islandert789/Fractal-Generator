660edb186331e8c325b14a8925beda4ee112b233
islandert789 <islandert789@yahoo.com>
2016-10-03 14:15:14 -0700

    Made the bounds code compatible with the class formatt

--- Fractal/fractal.py
+++ Fractal/fractal.py
@@ -2,36 +2,45 @@ import turtle
 import math
 
 class Fractal:
-    def __init(self, pattern, iterations, mirrored, angle):
+    def __init__(self, pattern, iterations, mirrored, angle):
         self.pattern = pattern
         self.iterations = iterations
         self.mirrored = mirrored
         self.angle = angle
+    def turns(self):
+        baseTurns = len(self.pattern)
+        turnN = baseTurns
+        for j in range(1,self.iterations+1):
+            turnN = baseTurns + turnN*baseTurns + turnN
+        return turnN
     def bounds(self):
         x=0.0
         y=0.0
         # the initial direction
-        d = self.angle
+        direction = self.angle
         # the bounding coordinates
         b=[0,0,0,0]
         # length of the base pattern
         l=len(self.pattern)+1
-        tempPattern = pattern.copy()
+        tempPattern = self.pattern.copy()
         if self.mirrored:
             tempPattern+=[None]
             for i in range(l-2,-1,-1):
                 tempPattern+=[-tempPattern[i]]
-        for i in range(1,turns(l-1,n)+1):
+        for i in range(1,self.turns()+1):
             while not i%l:
                 i//=l
-            if mir:
-                d+=tempPattern[i%(2*l)-1]
+            if self.mirrored:
+                direction+=tempPattern[i%(2*l)-1]
             else:
-                d+=tempPattern[i%l-1]
-            x+=math.cos(math.radians(d))
-            y+=math.sin(math.radians(d))
+                direction+=tempPattern[i%l-1]
+            x+=math.cos(math.radians(direction))
+            y+=math.sin(math.radians(direction))
             b[0]=min(b[0],x)
             b[1]=min(b[1],y)
             b[2]=max(b[2],x)
             b[3]=max(b[3],y)
         return [b[0]-1,b[1]-1,b[2]+1,b[3]+1]
+
+F = Fractal([90],10,True, 15)
+print(F.bounds())
